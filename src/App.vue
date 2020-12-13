f77788d2eef63560be99f837c0ffff02ee7b5365
Jackie Degl'Innocenti <info@jackied.it>
2020-12-13 20:45:47 +0100

    Improvements to navbar, added font

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,17 @@
 </template>
 
 <style lang="scss">
+
+@font-face {
+  font-family: "Andika New Basic";
+  src: local("AndikaNewBasic-Regular"),
+   url(./fonts/AndikaNewBasic/AndikaNewBasic-Regular.ttf) format("truetype");
+  font-weight: normal;
+  font-style: normal;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Andika New Basic", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
