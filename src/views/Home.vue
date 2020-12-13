dd75f4b74da25217b5e481a38f73b68c3bc12ad8
Jackie Degl'Innocenti <info@jackied.it>
2020-12-13 18:56:46 +0100

    Init of project

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Relocation Area Chooser</h1>
+    <p>Start View from Scratch</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Home',
+  components: {
+  },
+});
+</script>
