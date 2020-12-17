ae6ea14010ffa64ab596cc09e7c9948504df7b0a
Jackie Degl'Innocenti <info@jackied.it>
2020-12-17 12:27:02 +0100

    Created data fetch and store for flights

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
+import moment from 'moment';
+
 Vue.use(Vuex)
 
 const state = {
@@ -9,15 +11,18 @@ const state = {
   cities: [
     {
       'name': 'Amsterdam',
-      'accuweatherLocationId': 249758
+      'accuweatherLocationId': 249758,
+      'airportCode': 'AMS'
     },
     {
       'name': 'Madrid',
-      'accuweatherLocationId': 308526
+      'accuweatherLocationId': 308526,
+      'airportCode': 'MAD'
     },
     {
       'name': 'Budapest',
-      'accuweatherLocationId': 187423
+      'accuweatherLocationId': 187423,
+      'airportCode': 'BUD'
     }
   ]
 }
@@ -25,7 +30,6 @@ const state = {
 const actions = {
   initWeather({ commit, state }) {
     const accuweatherApiKey = process.env.VUE_APP_ACCUWEATHER_API_KEY || '';
-    console.log(accuweatherApiKey);
     for( let cityIndex in state.cities ) {
       const city = state.cities[cityIndex];
       const accuWeatherForecastUrl = 'http://dataservice.accuweather.com/currentconditions/v1/' + city.accuweatherLocationId;
@@ -46,19 +50,70 @@ const actions = {
         console.log(error);
       })
     }
+  },
+  initFlights({ commit, state }) {
+    const tequilaApiKey = process.env.VUE_APP_TEQUILA_API_KEY || '';
+    console.log('A', tequilaApiKey);
+    for( let cityIndex in state.cities ) {
+      const city = state.cities[cityIndex];
+      const tequilaApi = 'https://tequila-api.kiwi.com/v2/search';
+      const url = new URL(tequilaApi);
+      const dateFormatPattern = 'DD/MM/YYYY';
+      const todayDate = moment().format(dateFormatPattern);
+      const tomorrowDate = moment().add(1,'days').format(dateFormatPattern);
+
+      url.searchParams.set('fly_from', city.airportCode);
+      url.searchParams.set('date_from', todayDate);
+      url.searchParams.set('date_to', tomorrowDate);
+      url.searchParams.set('one_for_city', '1');
+      url.searchParams.set('flight_type', 'oneway');
+      url.searchParams.set('locale', 'en');
+      url.searchParams.set('curr', 'EUR');
+      url.searchParams.set('sort', 'price');
+      const axiosConfig = {
+        headers: {
+          'apikey': tequilaApiKey
+        }
+      };
+      axios.get( url.toString(), axiosConfig )
+      .then(response => {
+        const apiData = response.data.data;
+        console.log(apiData);
+        const flights = apiData.map(flight => ({
+          to: flight.cityTo+', '+flight.countryTo.name,
+          price: flight.price,
+          time: moment(flight.local_departure).format('DD MMM HH:mm'),
+          duration: moment(flight.duration.total * 1000).format('HH:mm')
+        }));
+        console.log(flights);
+        commit('SET_FLIGHTS', {cityIndex, flights})
+      })
+      .catch(error => {
+        alert('Network Error: API over limit or no connection');
+        console.log(error);
+      })
+    }
   }
 }
 
 const mutations = {
+  SET_NAME(state, name) {
+    state.name = name;
+  },
   SET_FORECAST(state, payload) {
     const cities = [...state.cities];
     cities[payload.cityIndex].forecast = payload.forecast;
     state.cities = cities;
   },
-  SET_NAME(state, name) {
-    state.name = name;
+  SET_FLIGHTS(state, payload) {
+    const cities = [...state.cities];
+    cities[payload.cityIndex].flights = payload.flights;
+    state.cities = cities;
   },
   CHOOSE_WEATHER(state, chosenCity) {
+    state.chosenCity = chosenCity;
+  },
+  CHOOSE_FLIGHTS(state, chosenCity) {
     state.chosenCity = chosenCity;
   }
 }
--- src/views/Flights.vue
+++ src/views/Flights.vue
@@ -1,23 +1,108 @@
 <template>
   <mdb-container fluid style="height: 100%">
     <navbar v-bind:showMenu="false" />
-    <div>
+        <section class="text-center">
+          <h2 class="h1 pt-4">
+            <mdb-icon fas icon="plane-arrival" />
+            {{ $t('flights.title') }}
+          </h2>
 
+          <p class="grey-text mb-5 mt-5 px-3">{{ $t('flights.desc') }}</p>
+        </section>
+    <mdb-row>
+      <mdb-col md="4" class="mt-2" v-for="(city, cityIndex) in cities" :key="city.name">
+          <mdb-card :style="chosenCity == cityIndex ? 'box-shadow: 0 2px 5px 0 rgba(0,0,255,0.70), 0 2px 10px 0 rgba(0,0,255,0.70)' : ''">
+            <mdb-card-image :src="require('@/assets/' + city.name.toLowerCase() + '.jpg')"></mdb-card-image>
+            <mdb-card-body>
+              <mdb-card-title>
+                {{ city.name }}
+              </mdb-card-title>
+              <mdb-card-text>
+                <p style="font-size: smaller;" class="grey-text">{{ $t('flights.current_weather') }}</p>
+                <img v-if="city.forecast" :src="city.forecast.iconUrl" />
+                <p class="mt-2 font-italic">{{ (city.forecast || {}).text || '...' }}, <span class="font-weight-bold">{{ (city.forecast || {}).temperature || '... °C' }}</span></p>
+                <p v-html="$t('weather.city_1_desc')"></p>
+              </mdb-card-text>
+              <mdb-btn color="info">
+                <mdb-icon fas icon="question" />
+                {{ $t('common.details') }}
+              </mdb-btn>
+              <mdb-btn color="primary" v-on:click="select(cityIndex)">
+                <mdb-icon fas icon="check-square" />
+                {{ $t('common.select') }}
+              </mdb-btn>
+              <div class="custom-control custom-checkbox custom-control-inline" style="position: absolute; bottom: 8px; right: -12px; z-index: 999;">
+                <input type="checkbox" class="custom-control-input" id="defaultInline1" :checked="chosenCity == cityIndex">
+                <label class="custom-control-label" for="defaultInline1"></label>
+              </div>
+              <div style="position: absolute; bottom: 0px; right: 0px; z-index: 1500; width: 40px; height: 40px;"></div>
+            </mdb-card-body>
+          </mdb-card>
+      </mdb-col>
+    </mdb-row>
+    <div class="mt-4 pb-4">
+      <mdb-btn color="primary" v-on:click="goToNext" :disabled="chosenCity === undefined"><mdb-icon icon="arrow-right" class="mr-1"/> {{ $t('flights.next_step') }}</mdb-btn>
     </div>
   </mdb-container>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 
-import { mdbContainer } from 'mdbvue';
-import navbar from '@/components/navbar.vue';
+  import Vue from 'vue';
+  import VueAxios from 'vue-axios';
 
-export default Vue.extend({
-  name: 'Cities',
-  components: {
-    mdbContainer,
-    navbar
-  },
-});
+  import navbar from '@/components/navbar.vue';
+  import { mdbCardImage, mdbCardText, mdbModal, mdbModalHeader, mdbModalTitle, mdbModalBody, mdbModalFooter, mdbContainer, mdbCard, mdbBtn, mdbRow, mdbCol, mdbCardUp, mdbCardAvatar, mdbCardBody, mdbCardTitle, mdbIcon, mdbInput } from 'mdbvue';
+  import router from '../router';
+
+  import store from '../store';
+  import axios from 'axios';
+
+  Vue.use(VueAxios, axios)
+
+  export default Vue.extend({
+    name: 'Flights',
+    data: function() {
+      return {
+        cities: store.state.cities, 
+        chosenCity: undefined
+      }
+    },
+    methods: {
+      goToNext: function() {
+        store.commit('CHOOSE_FLIGHTS', this.chosenCity);
+        router.push('resume');
+      },
+      select: function(cityIndex) {
+        this.chosenCity = cityIndex;
+      }
+    },
+    mounted: function() {
+      this.$store.dispatch('initFlights')
+        .then(() => {
+          console.log('flights init');
+        });
+    },
+    components: {
+      mdbContainer,
+      navbar,
+      mdbCard, 
+      mdbBtn,
+      mdbRow,
+      mdbCol,
+      mdbCardUp, 
+      mdbCardAvatar, 
+      mdbCardBody, 
+      mdbCardTitle, 
+      mdbIcon,
+      mdbInput,
+      mdbModal, 
+      mdbModalHeader, 
+      mdbModalTitle,
+      mdbModalBody, 
+      mdbModalFooter,
+      mdbCardImage,
+      mdbCardText
+    },
+  });
 </script>
